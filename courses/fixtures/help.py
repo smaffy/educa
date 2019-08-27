5e3fa415633a50269df60a6d62a472de7ed52d80
smaffy <jekrudcz@gmail.com>
2019-08-27 17:09:20 +0300

    Initial commit + field

---
+++ courses/fixtures/help.py
@@ -0,0 +1,15 @@
+"""
+
+dumpdata courses --indent=2 --output=courses/fixtures/subjects.json
+
+loaddata subjects.json
+
+Вы можете прочитать о том, как использовать fixtures для
+тестирования на https://docs.djangoproject.com/en/2.0/topics/testing/tools/#f
+ixture-loading .
+
+Вы
+можете найти документацию для переноса данных на https://docs
+.djangoproject.com/en/2.0/topics/migrations/#data-migrations .
+
+"""
