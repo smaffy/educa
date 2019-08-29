59badb5debf3fe105f953fca34be4401014eb9f4
smaffy <jekrudcz@gmail.com>
2019-08-29 16:31:28 +0300

    lists + details

--- courses/views.py
+++ courses/views.py
@@ -1,16 +1,17 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
-from .models import Module, Content
+from .models import Module, Content, Course, Subject
 from .forms import ModuleFormSet
-from .models import Course
 
 # больше о миксинах на https://docs.djangoproject.com/en/2.0/topics/class-based-views/mixins/ .
 # список всех миксинов, предоставленных django-braces на https://django-braces.readthedocs.io/ .
@@ -153,4 +154,21 @@ class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
             Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
 
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
